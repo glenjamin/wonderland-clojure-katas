1dd56a4d2ddf8aee422372a70fa181c225822847
Anders Engström <anders.engstrom@hrmsoftware.se>
2016-06-15 00:09:42 +0200

    wip: Finding a fn for checking allowed cases

--- fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
+++ fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
@@ -2,5 +2,11 @@
 
 (def start-pos [[[:fox :goose :corn :you] [:boat] []]])
 
+(defn allowed? [items]
+  (let [items (set items)]
+    (or (#{:you} items)
+        (not (clojure.set/subset? #{:fox :goose} items))
+        (not (clojure.set/subset? #{:corn :goose} items)))))
+
 (defn river-crossing-plan []
   start-pos)
