2a48c7a8b96ba6f3df19a8ffe0063fa7ff40605c
Anders Engström <anders.engstrom@hrmsoftware.se>
2016-06-15 22:53:51 +0200

    Brute-forcing the wonderland number

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,17 @@
 (ns wonderland-number.finder)
 
+(defn wonderland-number? [n]
+  (letfn [(same-digits? [n1]
+            (let [s1 (set (str n1))
+                  s2 (set (str n))]
+              (= s1 s2)))]
+    (and (same-digits? (* n 2))
+         (same-digits? (* n 3))
+         (same-digits? (* n 4))
+         (same-digits? (* n 5))
+         (same-digits? (* n 6)))))
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (->> (range 100000 1000000)
+       (filter wonderland-number?)
+       (first)))
