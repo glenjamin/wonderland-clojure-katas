8668299bb34d17d6cc658bb4f78a41592fa620ce
Anders Engström <anders.engstrom@hrmsoftware.se>
2016-06-28 21:08:45 +0200

    Playing around

--- tiny-maze/src/tiny_maze/solver.clj
+++ tiny-maze/src/tiny_maze/solver.clj
@@ -1,3 +1,20 @@
 (ns tiny-maze.solver)
 
-(defn solve-maze [maze])
+(defn value-at [maze x y]
+  ((maze y) x))
+
+(defn open? [maze x y]
+  (some? (#{:S 1 :E} (value-at maze x y))))
+
+(defn do-solve-maze [ctx maze])
+
+(defn find-start [maze]
+  (let [coords    (for [y (range (count maze))
+                        x (range (count (maze y)))] [x y])
+        predicate (fn [[x y]] (= :S (value-at maze x y)))]
+    (first (filter predicate coords))))
+
+(defn solve-maze [maze]
+  (do-solve-maze {:visited #{}
+                  :current (find-start maze)}
+                 maze))
